dd01504424a4813573c7af71250600ace738575e
Justin Liang <94037287+MRasavong@users.noreply.github.com>
2024-11-18 12:19:08 -1000

    Add UI changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import required modules
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import psycopg2
 import os
 import secrets
@@ -58,12 +58,8 @@ def visualization():
     # Extract meter names from the result (list of tuples)
     meter_names_list = [meter[0] for meter in meter_names]
 
-    selected_meter = None
-    if request.method == 'POST':
-        selected_meter = request.form.get('meter_name')
-
-    # Render the visualization template and pass meter names and selected meter
-    return render_template('visualization.html', meters=meter_names_list, selected_meter=selected_meter)
+    # Render the visualization template and pass meter names
+    return render_template('visualization.html', meters=meter_names_list)
 
 
 if __name__ == '__main__':
